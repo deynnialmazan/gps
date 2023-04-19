2b1db03ae42528f7683b36af56adcefd5516d1f1
Deynni Almazan <deynnialmazan@hotmail.com>
2023-04-19 11:47:40 -0500

    Last update

--- script/index.js
+++ script/index.js
@@ -1,19 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGV5bm5pYWxtYXphbiIsImEiOiJjbGcxMG1uZGExZ2psM3FwNjlyaWU3MXJ1In0.JVtgXQW2KTo8JQBsPxl-IA';
 
-//Display map
-
-/*
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    //Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/deynnialmazan/clg5c2xij000101mow29527lh', // style URL
-    center: [-97.151, 49.8517], // starting position [lng, lat]
-    zoom: 10 // starting zoom 
-});
-*/
-
-
-// Showing user's location:
 const trackingBtn = document.querySelector('.tracking-btn');
 
 function getLocation(position) {
@@ -52,11 +38,13 @@ const options = {
     enableHighAccuracy: true
 }
 
-// Event listener
 
+const timer = setInterval(function() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition
         (getLocation, errorHandler), { enableHighAccuracy: true };
     } else {
         console.log('Geolocation is not supported by your browser');
-    }
+    }    
+    clearInterval(timer);
+}, 2000);
